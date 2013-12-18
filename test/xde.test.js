5dabd259019b6b139e387eba42df46a611d5f72a
Gregers Rygg <gregersrygg@gmail.com>
2013-12-18 14:20:40 +0100

    Allow listeners for multiple event names, and added off method to remove listener.

--- test/xde.test.js
+++ test/xde.test.js
@@ -2,7 +2,7 @@ suite('xde', function () {
 	var iframe, childXde, postMessage;
 	var iframeContent = '<!DOCTYPE html><html><head><script src=\'/base/lib/xde.js?'+new Date().getTime()+'\'></script><script>parent.setChildXde(xde);</script></head><body>test</body></html>';
 
-	function asyncDone(fn, done) {
+	function async(fn, done) {
 		return function () {
 			try {
 				fn.apply(this, arguments);
@@ -112,11 +112,22 @@ suite('xde', function () {
 		});
 
 		test('should trigger listener on the other side', function (done) {
-			var eventName = 'foo',
-				eventData = 'abc' + new Date().getTime();
-			childXde.on(eventName, asyncDone(function (evt) {
+			var eventName = 'foo';
+			childXde.on(eventName, async(function (evt) {
 				assert.defined(evt);
 				assert.equals(evt.name, eventName);
+			}, done));
+
+			xde.sendTo(iframe, eventName);
+		});
+
+		test('should send event data to the listener', function (done) {
+			var eventName = 'foo',
+				eventData = {
+					time: new Date().getTime(),
+					str: 'abc'
+				};
+			childXde.on(eventName, async(function (evt) {
 				assert.equals(evt.data, eventData);
 			}, done));
 
@@ -128,9 +139,97 @@ suite('xde', function () {
 			iframe.contentWindow.onerror = spy;
 			iframe.contentWindow.postMessage('not serialized JSON', '*');
 
-			setTimeout(asyncDone(function () {
+			setTimeout(async(function () {
 				sinon.assert.notCalled(spy);
 			}, done), 50);
+		});
+
+		test('should ignore json messages not sent by xde', function (done) {
+			var spy = sinon.spy();
+			var data = {data : 'foo', name : 'bar'};
+			childXde.on(data.name, spy);
+			iframe.contentWindow.postMessage(JSON.stringify(data), '*');
+
+			setTimeout(async(function () {
+				sinon.assert.notCalled(spy);
+			}, done), 50);
+		});
+
+		test('should allow multiple listeners for same event name', function (done) {
+			var spy1 = sinon.spy(), spy2 = sinon.spy();
+			var evtName = 'multiple';
+			childXde.on(evtName, spy1);
+			childXde.on(evtName, spy2);
+			xde.sendTo(iframe, evtName);
+			
+			setTimeout(async(function () {
+				sinon.assert.calledOnce(spy1);
+				sinon.assert.calledOnce(spy2);
+			}, done), 50);
+		});
+
+		test('should only trigger listeners for the sent event name', function (done) {
+			var spy1 = sinon.spy(), spy2 = sinon.spy();
+			var evtName1 = 'foo';
+			var evtName2 = 'bar';
+			childXde.on(evtName1, spy1);
+			childXde.on(evtName2, spy2);
+			xde.sendTo(iframe, evtName1);
+			
+			setTimeout(async(function () {
+				sinon.assert.calledOnce(spy1);
+				sinon.assert.notCalled(spy2);
+			}, done), 50);	
+		});
+
+		test('should not throw error or call any callbacks if no listners for an event name', function (done) {
+			var spy = sinon.spy();
+			iframe.contentWindow.onerror = spy;
+			
+			childXde.on('foo', spy);
+			xde.sendTo(iframe, 'bar');
+			setTimeout(async(function () {
+				sinon.assert.notCalled(spy);
+			}, done), 50);
+		});
+	});
+
+	suite('off', function () {
+		test('should not throw exception when no listeners for given event name', function () {
+			refute.exception(function () {
+				xde.off('foo', function () {});
+			});
+		});
+
+		test('should remove listener for given event name', function (done) {
+			var spy = sinon.spy();
+			var eventName = 'foo';
+
+			childXde.on(eventName, spy);
+			childXde.off(eventName, spy);
+
+			xde.sendTo(iframe, eventName);
+			setTimeout(async(function () {
+				sinon.assert.notCalled(spy);
+			}, done), 50);
+		});
+
+		test('should only remove the given listener on a particular event name', function (done) {
+			var spy1 = sinon.spy(), spy2 = sinon.spy();
+			var eventName = 'foo';
+
+			childXde.on(eventName, spy1);
+			childXde.on(eventName, spy2);
+
+			childXde.off(eventName, spy1);
+
+			xde.sendTo(iframe, eventName);
+
+			setTimeout(async(function () {
+				sinon.assert.calledOnce(spy2);
+				sinon.assert.notCalled(spy1);
+			}, done), 50);	
+
 		});
 	});
 });
