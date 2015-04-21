95260b26d5923546795c26bcd88895abd26bcbe7
sveisvei <sveinung.rosaker@gmail.com>
2015-04-21 11:53:30 +0200

    clean settings

--- karma.conf.js
+++ karma.conf.js
@@ -8,37 +8,32 @@ function add(b, p, ver, device) {
     };
 }
 
-function addAndroid(output) {
-    output['android_4'] = add('android', 'Linux', '4.0', 'Android Emulator');
-    output['android_4-4'] = add('android', 'Linux', '4.4', 'Google Nexus 7 HD Emulator');
-}
-
-function addIOS(output) {
-    output['ios_safari_7-1'] = add('iphone', 'OS X 10.9', '7.1', 'iPhone Simulator');
-    // output['ios_safari_8-0'] = add('iphone', 'OS X 10.10', '8.0', 'iPhone Simulator');
-    output['ios_safari_8-2'] = add('iphone', 'OS X 10.10', '8.2', 'iPhone Simulator');
-}
-
-function addChrome(output) {
-    // output['chrome_35'] = add('chrome', 'Windows 7', '35');
-    output['chrome_41'] = add('chrome', 'Windows 8.1', '41');
-    output['chrome_beta'] = add('chrome', 'Windows 8.1', 'beta');
-}
-
-function addFirefox(output) {
-    output['firefox_30'] = add('firefox', 'Windows 8', '30');
-    output['firefox_37'] = add('firefox', 'Windows 8.1', '37');
-}
-
-function addIENew(output) {
-    output['ie_11'] = add('internet explorer', 'Windows 8.1', '11');
-    output['ie_10'] = add('internet explorer', 'Windows 8', '10');
-}
-
-function addIE(output) {
-    output['ie_09'] = add('internet explorer', 'Windows 7', '9');
-    output['ie_08'] = add('internet explorer', 'Windows 7', '8');
-}
+var browsers = {
+    'android': {
+        'android_4': add('android', 'Linux', '4.1', 'Android Emulator'),
+        'android_4-4': add('android', 'Linux', '4.4', 'Google Nexus 7 HD Emulator')
+    },
+    'ios': {
+        'ios_safari_7-1': add('iphone', 'OS X 10.9', '7.1', 'iPhone Simulator'),
+        'ios_safari_8-2': add('iphone', 'OS X 10.10', '8.2', 'iPhone Simulator')
+    },
+    'chrome': {
+        'chrome_41': add('chrome', 'Windows 8.1', '41'),
+        'chrome_beta': add('chrome', 'Windows 8.1', 'beta')
+    },
+    'firefox': {
+        'firefox_30': add('firefox', 'Windows 8', '30'),
+        'firefox_37': add('firefox', 'Windows 8.1', '37')
+    },
+    'ienew': {
+        'ie_11': add('internet explorer', 'Windows 8.1', '11'),
+        'ie_10': add('internet explorer', 'Windows 8', '10')
+    },
+    'ie': {
+        'ie_09': add('internet explorer', 'Windows 7', '9'),
+        'ie_08': add('internet explorer', 'Windows 7', '8')
+    }
+};
 
 module.exports = function(config) {
     var settings = {
@@ -84,22 +79,16 @@ module.exports = function(config) {
         // only 3 vmms / browsers per run because of
         // https://github.com/karma-runner/karma-sauce-launcher/issues/40
         // should either add better setup for running max currently or
-        if (process.env.BROWSER_TYPE === 'android') {
-            addAndroid(settings.customLaunchers);
-        } else if (process.env.BROWSER_TYPE === 'ios') {
-            addIOS(settings.customLaunchers);
-        } else if (process.env.BROWSER_TYPE === 'chrome') {
-            addChrome(settings.customLaunchers);
-        } else if (process.env.BROWSER_TYPE === 'firefox') {
-            addFirefox(settings.customLaunchers);
-        } else if (process.env.BROWSER_TYPE === 'ie') {
-            addIE(settings.customLaunchers);
-        } else if (process.env.BROWSER_TYPE === 'ienew') {
-            addIENew(settings.customLaunchers);
-        } else {
+        var key = process.env.BROWSER_TYPE;
+        var target = browsers[key];
+        if (!target) {
             console.error('Missing / Unknown pass ' + process.env.PASS);
             process.exit(1);
         }
+
+        Object.keys(target).forEach(function(key){
+            settings.customLaunchers[key] = target[key];
+        });
 
         console.log('Running CI tests on', Object.keys(settings.customLaunchers).join(', '));
         settings.browsers = Object.keys(settings.customLaunchers);
